c8be59d77e53bc1d9802234382decec02bf2b6cb
Ivan Marelli <ivanmarelli@gmail.com>
2024-04-03 13:35:43 -0300

    creacion migraciones

---
+++ database/migrations/2024_04_03_160422_create_order_lines_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id')->unsigned()->index();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->integer('qty')->default(1); 
+            $table->unsignedBigInteger('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_lines');
+    }
+};

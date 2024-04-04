3e76fd29972139ee5e990c7cab69082be4037ed0
Ivan Marelli <ivanmarelli@gmail.com>
2024-04-04 15:08:43 -0300

    fix si existe executed

--- resources/views/livewire/last-executed.blade.php
+++ resources/views/livewire/last-executed.blade.php
@@ -1,4 +1,5 @@
 <div wire:poll.120s>
+    @if($executed)
     <p><strong>Executed:</strong> 
 
     <small class="fst-italic">
@@ -6,5 +7,6 @@
         - Total: <span class="badge text-bg-secondary">{{ $executed->total_cost }}</span>
         - {{ $executed->created_at }}</small>
     </p>
+    @endif
     
 </div>
